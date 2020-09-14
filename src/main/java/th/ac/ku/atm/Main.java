499de46815495deb17ffd8e41354301b30ce87dc
Wipawee Wanwattana <wipawee.wanw@ku.th>
2020-09-14 23:32:33 +0700

    initial commit

---
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -0,0 +1,23 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml");
+
+        ATM atm = context.getBean(ATM.class);
+
+        String name = atm.validateCustomer(1,5555);
+        System.out.println(name + " has " +atm.getBalance());
+        atm.withdraw(200);
+        System.out.println(name + " has " +atm.getBalance());
+
+        Bank bank =context.getBean(Bank.class);
+        System.out.println(bank.getName());
+    }
+}
